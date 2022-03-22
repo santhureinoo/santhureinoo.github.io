4607cfc4913166bff6126191e43f8248b3dead92
santhureinoo <santhueinoo@gmail.com>
2022-03-22 09:17:38 +0000

    Deploying to gh-pages from @ santhureinoo/santhureinoo.github.io@4947107a950d8d9e7b3191dcc88bc89d38ed5e02 🚀

--- _next/static/JQesdX1oSClmU-WOGclJW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/211-151ce93549e2b35b.js","static/chunks/pages/index-55a99b83f38f8c32.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
