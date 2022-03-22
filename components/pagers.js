6c34acfca4320f06bb49ad3af4bf2fdfd7e481dc
SanThureinOo <santhueinoo@gmail.com>
2022-03-22 14:29:42 +0630

    Trigger to rebuild workflow

--- components/pagers.js
+++ components/pagers.js
@@ -16,6 +16,7 @@ export default function Pager() {
           fill="currentColor"
           aria-hidden="true"
         >
+          
           <path
             fill-rule="evenodd"
             d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
